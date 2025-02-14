652383a77981ac5f1530fd5e162770ccab71974f
Nandini <74641697+nandiniguptaz@users.noreply.github.com>
2025-02-14 20:00:24 +0530

    upload val

---
+++ script.js
@@ -0,0 +1,23 @@
+// let noClicks = 0;
+
+// document.getElementById('btnNo').addEventListener('click', function() {
+//     noClicks++;
+//     document.getElementById('btnYes').style.fontSize = `${noClicks * 50 + 16}px`;
+    
+//     // if (noClicks === 5) {
+//     //   document.getElementById('resultText').textContent = 'YES!';
+//     // }
+
+
+//   });
+
+
+
+// function func1() {
+
+// }
+
+// let vid = document.getElementById("myVideo");
+
+// vid.loop = true;
+// vid.load();
